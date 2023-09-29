081ffb4861da315af264a09228ca214a8a727d6b
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-09-29 10:03:30 -0300

    first commit

---
+++ Musica.cs
@@ -0,0 +1,9 @@
+// criando classe com propriedades tipadas para usar em outros arquivos adiciono o puplic antes da propriedade pra diser que essa propriedade é pplica
+
+class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// chamo minha calse musica como se fosse um tipo pra por em uma variavel
+// esse new musica quer dizer que estamos criando um objeto especifico de tipo Musica
+
+Musica musica1 = new Musica();
+musica1.nome = "Roxane";
+musica1.artista = "The Police";
+
+Console.WriteLine($"Nome da musica {musica1.nome}");
+Console.WriteLine($"Nome do artista é {musica1.artista}");
+
+
+Musica musica2 = new Musica();
+musica2.nome = "Vertigo";
+musica2.artista = "U2";
+
+Console.WriteLine($"Nome da musica {musica2.nome}");
+Console.WriteLine($"Nome do artista é {musica2.artista}");
