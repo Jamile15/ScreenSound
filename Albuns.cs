72dc9fff988f374218f8c2ca6a08d9aba286bcdb
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-10-04 09:35:15 -0300

    criando uma nova class

---
+++ Albuns.cs
@@ -0,0 +1,12 @@
+//criando albun
+
+class Albuns
+{
+    public string Nome { get; set; }
+    public int Duracao { get; set; }
+
+    public void AficionadoMusica( Musica musica);
+    {
+
+    }
+}
